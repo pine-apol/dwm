8ebe8e4b47d1762efe519126176b83e3ac7f9f86
pine-apol <pine2cey@protonmail.com>
2024-08-04 23:46:25 +0800

    Audio Function with F Keys

--- config.def.h
+++ config.def.h
@@ -105,6 +105,12 @@ static const Key keys[] = {
 	TAGKEYS(                        XK_9,                      8)
 	{ MODKEY|ShiftMask,             XK_q,      quit,           {0} },
 	{ MODKEY,                       XK_w,      spawn,          {.v = browsercmd } },
+	{ 0, XK_F7, spawn, SHCMD("pactl set-sink-volume @DEFAULT_SINK@ -5%") },
+        { 0, XK_F8, spawn, SHCMD("pactl set-sink-volume @DEFAULT_SINK@ +5%") },
+        { 0, XK_F9, spawn, SHCMD("pactl set-sink-mute @DEFAULT_SINK@ toggle") },
+        { 0, XK_F10, spawn, SHCMD("pactl set-sink-volume @DEFAULT_SINK@ 50%") },
+        { 0, XK_F11, spawn, SHCMD("pactl set-sink-volume @DEFAULT_SINK@ 100%") },
+
 };
 
 /* button definitions */
